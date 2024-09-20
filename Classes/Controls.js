7da6b12b3c40722969de316ac730e965050d376f
Yafarrodrigo <yafarrodrigo@yahoo.com.ar>
2024-09-20 20:11:51 -0300

    minor fixes in controls

--- Classes/Controls.js
+++ Classes/Controls.js
@@ -10,13 +10,12 @@ export default class Controls{
     }
 
     createListeners(game){
-        document.addEventListener('keydown', function(e){
+        document.addEventListener('keydown', (e) => {
             if(this.lastKey === e.key){
                 console.log("nope!");
             }else{
                 console.log(e.key);
                 this.lastKey = e.key
-                
             }
 
             if(this.cooldown === true) return
@@ -24,63 +23,55 @@ export default class Controls{
             this.cooldown = true
             setTimeout(() => this.cooldown = false, 50)
             
-            if((e.key !== "m" && e.key !== "M") && game.mode === "open map") return
-            switch(e.key){
-
-                case "g":
-                case "G":
-                    if(this.cooldownSave === true){
-                        game.log.info("saving in cooldown (once every 15s)")
-                        return
-                    }
-
-                    this.cooldownSave = true
-                    setTimeout(() => this.cooldownSave = false, 15000)
-
+            // moving controls
+            if(game.mode === "moving player"){
+                moveControls(game, e.key)
+            }
+            
+            // save game
+            if(e.key === "G" || e.key === "g"){
+                if(this.cooldownSave === true){
+                    game.log.info("saving in cooldown (once every 15s)")
+                }else{
                     game.saveGame()
                     game.log.info("saving game...")
-                    break;
-            
-                case "m":
-                case "M":
-                    if(this.cooldownMap === true) return
+                    this.cooldownSave = true
+                    setTimeout(() => this.cooldownSave = false, 15000)
+                }
+            }
 
+            // open/close map
+            if(e.key === "m" || e.key === "M"){
+                if(this.cooldownMap === false){
                     this.cooldownMap = true
-                    setTimeout(() => this.cooldownMap = false, 250)
-
+                    setTimeout(() => this.cooldownMap = false, 500)
+    
                     if(game.mode === "open map"){
                         game.mode = "moving player"
                     }else{
                         game.mode = "open map"
                     }
                     game.update()
-                    break;
-                case "ArrowLeft":
-                    if(game.mode === "moving player"){
-                        game.movePlayer("left")
-                    }
-                    game.update()
-                    break;
-                case "ArrowRight":
-                    if(game.mode === "moving player"){
-                        game.movePlayer("right")
-                    }
-                    game.update()
-                    break;
-                case "ArrowUp":
-                    if(game.mode === "moving player"){
-                        game.movePlayer("up")
-                    }
-                    game.update()
-                    break;
-                case "ArrowDown":
-                    if(game.mode === "moving player"){
-                        game.movePlayer("down")
-                    }
-                    game.update()
-                    break;
+                }
             }
         })
-
     }
+}
+
+function moveControls(game, key){
+    switch(key){
+        case "ArrowLeft":
+            game.movePlayer("left")
+            break;
+        case "ArrowRight":
+            game.movePlayer("right")
+            break;
+        case "ArrowUp":
+            game.movePlayer("up")
+            break;
+        case "ArrowDown":
+            game.movePlayer("down")
+            break;
+    }
+    game.update()
 }
